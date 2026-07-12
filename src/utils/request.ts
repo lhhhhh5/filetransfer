297252560a6f495743ad3e7de9ef71a5044b8979
lhh <2652926623@qq.com>
2026-07-12 21:21:02 +0800

    file():处理了一些线上问题
    1.访问不了后端，对util的后端地址进行了修改

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 export const request = axios.create({
-  baseURL: 'http://localhost:8080/fileTransferApi/file',
+  baseURL: 'http://121.196.218.190:8080/fileTransferApi/file',
   timeout: 10000
 });
 
 export const toMinio = axios.create({
-  baseURL: 'http://192.168.154.100:9000/',
+  baseURL: 'http://121.196.218.190:9000/',
   timeout: 10000
 });
